072fbd9d0d0c76d66b5bf9f971c1d8a31c6e3512
Aluno <aluno@ifslab.edu.br>
2020-01-15 17:20:28 -0300

    Direcionar para Home ou Login

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php include_once 'header.php'; ?>
 
 <div class="body-container">
-    corpo
+    <?php
 
-<br>
+    $pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 'home';
 
-    psé né
+    switch ($pagina) {
+        case 'login':
+            include_once 'view/login.php';
+            break;
+        case 'home':
+            include_once 'view/home.php';
+            break;
+        default:
+            include_once 'view/home.php';
+    }
+    ?>
 </div>
 
 <?php include_once 'footer.php'; ?>
