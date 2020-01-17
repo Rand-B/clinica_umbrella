6707d1a15ec52ca207a504e96db4c5131c6c359b
Aluno <aluno@ifslab.edu.br>
2020-01-17 17:31:11 -0300

    JS para girar a logo

--- view/home.php
+++ view/home.php
@@ -8,3 +8,21 @@
     <img id="img-home" src="img/umbrella.png" alt="Logo">
     
 </div>
+
+<script>
+    let deg = 0;
+    let img_home = document.getElementById("img-home")
+
+    // Faz o javascript esperar por uma certa quantidade de milisegundos
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+
+    (async function girarImagem() {
+        img_home.style.transform = `rotate(${deg}deg)`
+        deg = deg >= 360 ? 0 : deg + 1
+        await sleep(23)
+        girarImagem()
+    })()
+</script>
