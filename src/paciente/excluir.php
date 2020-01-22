70c0928239db1f7bb140ee4709913c0805c5cdea
Aluno <aluno@ifslab.edu.br>
2020-01-22 17:53:34 -0300

    Excluir paciente

---
+++ src/paciente/excluir.php
@@ -0,0 +1,14 @@
+<?php
+
+    // conexão com o banco
+    require 'src/conexao-com-banco.php';
+
+    // pegar ID
+    $pacienteId = isset($_GET['id']) ? $_GET['id'] : 0;
+
+    // tentar excluir do BD
+    $sql = "DELETE FROM `paciente` WHERE id = $pacienteId;";
+    mysqli_query($conexao, $sql);
+
+    // retornar para a página pacientes
+    header('location: http://localhost/clinica_umbrella/index.php?pagina=pacientes');
