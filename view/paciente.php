1930a8ac017de7d346f519a888bc9e2354f307ed
Aluno <aluno@ifslab.edu.br>
2020-01-22 17:54:54 -0300

    Tabela de pacientes

---
+++ view/paciente.php
@@ -0,0 +1,32 @@
+<?php 
+
+    require 'src/paciente/listar.php'; ?>
+
+    <br><br><br>
+    <table class="table table-bordered table-hover table-striped" style="width: 100%"  id="tabela-pacientes">
+        <thead>
+        <tr>
+            <th>Nome</th>
+            <th>Data de Nascimento</th>
+            <th>CPF</th>
+            <th>Ações</th>
+        </tr>
+        </thead>
+        <body>
+    
+    <?php while ($linha = mysqli_fetch_assoc($tabelaPacientes)) { ?>
+
+    <tr>
+        <td><?= $linha['nome'] ?></td>
+        <td><?= $linha['data_nascimento'] ?></td>
+        <td><?= $linha['cpf'] ?></td>
+        <td>
+            <a href="?pagina=salvarPaciente&id=<?= $linha['id'] ?>">Editar</a>
+            |
+            <a href="src/paciente/excluir.php?id=<?= $linha['id'] ?>">Excluir</a>
+        </td>
+    </tr>
+
+<?php } ?>
+    </body>
+</table>
