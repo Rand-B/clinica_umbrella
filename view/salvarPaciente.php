67376aeed41a87e2611f098283c73ac7111ad699
Aluno <aluno@ifslab.edu.br>
2020-01-24 18:11:23 -0300

    Implementações para tabela

--- view/salvarPaciente.php
+++ view/salvarPaciente.php
@@ -1,20 +1,25 @@
+<?php require 'src/paciente/pesquisarPorId.php' ?>
 <div class="container">
         <h2> Salvar Paciente </h2> 
-        <form action="src/paciente/salvar.php" method="post">
+        <form action="src/paciente/salvar.php<?= isset($_GET['id']) ? '?' . $_GET['id'] : ''?>" method="post">
 
+        <?php if (isset($_GET['id'])) { ?>
+            <input type="hidden" name="id" value="<?= $_GET['id']?>">
+        <?php } ?>
+        
         <div class="form-group">
             <label for="input_nome">Nome</label>
-            <input required class="form-control" type="text" name="nome" id="input_nome">
+            <input value="<?= $paciente['nome'] ?>" required class="form-control" type="text" name="nome" id="input_nome">
         </div>
 
         <div class="form-group">
             <label for="input_data">Data de Nascimento</label>
-            <input required class="form-control" type="date" name="data_nascimento" id="input_data_nascimento">
+            <input value="<?= $paciente['data_nascimento'] ?>"required class="form-control" type="date" name="data_nascimento" id="input_data_nascimento">
         </div>
 
         <div class="form-group">
             <label for="input_cpf">CPF</label>
-            <input required class="form-control" type="text" name="cpf" id="input_cpf">
+            <input value="<?= $paciente['cpf'] ?>"required class="form-control" type="text" name="cpf" id="input_cpf">
         </div>
 
             <?php if (isset($_GET['erro'])) { ?>
